325b8dd62b7e98fabb9a9b6731244752e1b2ed2f
Lukas Striebel <lukas.striebel@hotmail.com>
2019-08-01 10:09:27 +0200

    Fix typo in About Me

--- src/components/aboutMe/AboutMe.tsx
+++ src/components/aboutMe/AboutMe.tsx
@@ -96,9 +96,9 @@ export default class AboutMe extends Component<IProps, IState> {
 function Background() {
     return (
         <React.Fragment>
-            <div className={`what-sets-me-apart about-me-item`}>
+            <div className={`backgound`}>
                 <h3 className={`heading-secondary`}>Where I am coming from</h3>
-                <p>I learned programming with Java at the young age of 15. Fascinated by the sheer endless possibilties of coding,
+                <p>I learned programming with Java at the young age of fifteen. Fascinated by the sheer endless possibilties of coding,
                     I decided to study Computer Science at the <span className={`highlight`}>Swiss Federal Institute of Technology</span>.
                      In 2017 I graduate with a Master's Degree and have been working as Software Engineer ever since.</p>
             </div>
@@ -109,27 +109,27 @@ function Background() {
 function Work() {
     return (
         <React.Fragment>
-            <div className={`my-practices about-me-item`}>
-                <div className={`my-practices__item`}>
+            <div className={`work about-me-item`}>
+                <div className={`work__item`}>
                     <h3 className={`heading-secondary`}>Frontend</h3>
                     <p>As a big fan of <span className={`highlight`}>React Native</span>, I try to use it whenever possible. The advantage of
                     having the same code run on iOS, Android and Web is unbeatable.</p>
                 </div>
-                <div className={`my-practices__item`}>
+                <div className={`work__item`}>
                     <h3 className={`heading-secondary`}>Backend</h3>
-                    <p>Having more than 10 years of practical experience with Java, I write most of my backends as <span className={`highlight`}>RESTful API</span>, supoorted by either the Spring or EJB Framework.
-                        </p>
+                    <p>Having more than 10 years of practical experience with Java, I write most of my backends as <span className={`highlight`}>RESTful API</span>, supported by either the Spring or EJB Framework.
+                    </p>
                 </div>
-                <div className={`my-practices__item`}>
+                <div className={`work__item`}>
                     <h3 className={`heading-secondary`}>Architecture</h3>
                     <p>Scaling problem are some of the most common and yet the most difficult ones to face. Currently I am involved in designing an Architecture
                         that can handle huge temporary load spikes while still having low cost during off times.
                     </p>
                 </div>
-                <div className={`my-practices__item`}>
+                <div className={`work__item`}>
                     <h3 className={`heading-secondary`}>Mentoring</h3>
-                    <p>I enjoy teaching juniors the importance of well-written and functional Unit tests. It took me a long time to realize
-                        that the upfront time investment is always worth it in the long run.
+                    <p>I enjoy teaching junior engineers the importance of well-written code and functional unit tests. It took me a long time to realize
+                        that the upfront time investment is always pays dividends in the long run.
                     </p>
                 </div>
             </div>
@@ -140,11 +140,11 @@ function Work() {
 function Passion() {
     return (
         <React.Fragment>
-            <div className={`how-you-will-help about-me-item`}>
+            <div className={`passion about-me-item`}>
                 <h3 className={`heading-secondary`}>Code is art</h3>
                 <p>The one thing I enjoy more than writing code is actually deleting it.
-                    Clean Code is an art of itself. Unfortunately, a lack of time and skill often leads to a completely convoluted and unreadable
-                    mess (Spaghetti code), making bugs almost impossible to spot. Thus, nothing makes me happier than taking an ugly piece of legacy code and turn it into
+                    Writing <span className={`highlight`}>Clean Code</span> is an art in itself. Unfortunately, a lack of time and skill often leads to a completely convoluted and unreadable
+                    mess (Spaghetti code), making bugs almost impossible to spot. Thus, nothing makes me happier than taking an ugly piece of legacy code and turning it into
                     a nice <span className={`highlight`}>DRY</span> and <span className={`highlight`}>SOLID</span> class.
                     </p>
             </div>
