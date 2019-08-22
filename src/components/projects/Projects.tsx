f2f27bbfd92bf0499eb5a4cc2f8a93c8daf84c8e
Lukas Striebel <lukas.striebel@hotmail.com>
2019-08-22 21:33:10 +0200

    Rearrange projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -10,16 +10,16 @@ export default class Projects extends Component {
             <section id={`projects`} className={`projects`}>
                 <h1 className={`heading`}>Projects</h1>
                 <div className={`projects__grid`}>
-
                     <div className={`project`}>
                         <div className={`col-1`}>
-                            <h3 className={`heading-secondary`}>Personal Portfolio</h3>
+                            <h3 className={`heading-secondary`}>Movie Database</h3>                            
                             <div className={`project__display`}>
                                 <div className={`project__display__poster`}>
-                                    <img src={personal_site} alt={`Project Thumbnail`} />
+                                    <img src={movies} alt={`Project Thumbnail`} />
                                 </div>
                                 <div className={`project__display__actions`}>
-                                    <a href={`https://github.com/lukasstriebel/lukasstriebel.github.io/tree/development`} className={`link`}><i className="fas fa-code-branch"></i><span>View Source</span></a>
+                                    <a href={`https://myimdb.netlify.com/`} className={`link`}><i className="fas fa-desktop"></i><span>Live Site</span></a>
+                                    <a href={`https://github.com/lukasstriebel/movies`} className={`link`}><i className="fas fa-code-branch"></i><span>View Source</span></a>
                                 </div>
                             </div>
                             <div className={`project__technologies`}>
@@ -27,17 +27,21 @@ export default class Projects extends Component {
                                 <ul className={`project__technologies__list`}>
                                     <li><i className="fas fa-dot-circle"></i><p>ReactJS</p></li>
                                     <li><i className="fas fa-dot-circle"></i><p>NodeJS</p></li>
-                                    <li><i className="fas fa-dot-circle"></i><p>Typescript</p></li>
+                                    <li><i className="fas fa-dot-circle"></i><p>Javascript</p></li>
+                                    <li><i className="fas fa-dot-circle"></i><p>Axios</p></li>
+                                    <li><i className="fas fa-dot-circle"></i><p>AWS</p></li>
                                     <li><i className="fas fa-dot-circle"></i><p>HTML5</p></li>
                                     <li><i className="fas fa-dot-circle"></i><p>CSS3</p></li>
-                                    <li><i className="fas fa-dot-circle"></i><p>Sass</p></li>
                                 </ul>
                             </div>
                         </div>
                         <div className={`col-2`}>
                             <div className={`project__detail-section`}>
                                 <p className={`small-section-title`}>Overview</p>
-                                <p>Simple <span className={`highlight`}>React</span> project to display my portfolio of work. It was built using <span className={`highlight`}>React and Typescript</span></p>
+                                <p>A database of movies like IMDb. Built with <span className={`highlight`}>React and Javascript</span>.
+                                <br></br>
+                                It uses <span className={`highlight`}>Axios</span> and <span className={`highlight`}>AWS Lambda</span> functions to retrieve the latest information on movies from the backend server.
+                                <br></br>The site is currently hosted on Netlify.</p>
                             </div>
                         </div>
                     </div>
@@ -77,14 +81,13 @@ export default class Projects extends Component {
                     <hr/>
                     <div className={`project`}>
                         <div className={`col-1`}>
-                            <h3 className={`heading-secondary`}>Movie Database</h3>                            
+                            <h3 className={`heading-secondary`}>Personal Portfolio</h3>
                             <div className={`project__display`}>
                                 <div className={`project__display__poster`}>
-                                    <img src={movies} alt={`Project Thumbnail`} />
+                                    <img src={personal_site} alt={`Project Thumbnail`} />
                                 </div>
                                 <div className={`project__display__actions`}>
-                                    <a href={`https://myimdb.netlify.com/`} className={`link`}><i className="fas fa-desktop"></i><span>Live Site</span></a>
-                                    <a href={`https://github.com/lukasstriebel/movies`} className={`link`}><i className="fas fa-code-branch"></i><span>View Source</span></a>
+                                    <a href={`https://github.com/lukasstriebel/lukasstriebel.github.io/tree/development`} className={`link`}><i className="fas fa-code-branch"></i><span>View Source</span></a>
                                 </div>
                             </div>
                             <div className={`project__technologies`}>
@@ -92,26 +95,20 @@ export default class Projects extends Component {
                                 <ul className={`project__technologies__list`}>
                                     <li><i className="fas fa-dot-circle"></i><p>ReactJS</p></li>
                                     <li><i className="fas fa-dot-circle"></i><p>NodeJS</p></li>
-                                    <li><i className="fas fa-dot-circle"></i><p>Javascript</p></li>
-                                    <li><i className="fas fa-dot-circle"></i><p>Axios</p></li>
-                                    <li><i className="fas fa-dot-circle"></i><p>AWS</p></li>
+                                    <li><i className="fas fa-dot-circle"></i><p>Typescript</p></li>
                                     <li><i className="fas fa-dot-circle"></i><p>HTML5</p></li>
                                     <li><i className="fas fa-dot-circle"></i><p>CSS3</p></li>
+                                    <li><i className="fas fa-dot-circle"></i><p>Sass</p></li>
                                 </ul>
                             </div>
                         </div>
                         <div className={`col-2`}>
                             <div className={`project__detail-section`}>
                                 <p className={`small-section-title`}>Overview</p>
-                                <p>A database of movies like IMDb. Built with <span className={`highlight`}>React and Javascript</span>.
-                                <br></br>
-                                It uses <span className={`highlight`}>Axios</span> and <span className={`highlight`}>AWS Lambda</span> functions to retrieve the latest information on movies from the backend server.
-                                <br></br>The site is currently hosted on Netlify.</p>
+                                <p>Simple <span className={`highlight`}>React</span> project to display my portfolio of work. It was built using <span className={`highlight`}>React and Typescript</span></p>
                             </div>
                         </div>
                     </div>
-                    
-
                 </div>
             </section>
         )
